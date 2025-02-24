89b09f86b11f26665deab4b4592d1748d15a1a76
MariaLuizaCavalcanti <maria.luiza.andrade.cavalcanti@ccc.ufcg.edu.br>
2025-02-24 12:01:07 -0300

    fazendo

--- src/Rodada.hs
+++ src/Rodada.hs
@@ -1,8 +1,28 @@
 module Rodada where
 
--- [numJogadores, numBots], objetivoSorteado, mapaSorteado, 
-rodada::[Int]->[Int]->[[Int]]->IO()
-rodada jogadoresInfo objetivos mapa = do
+-- [numJogadores, numBots], objetivoSorteado, índice do jogador da vez, mapaSorteado, 
+rodada::[Int]->[Int]->Int->[[Int]]->IO()
+rodada jogadoresInfo objetivos indiceJogador mapa = do
     print mapa
+    putStrLn $ "Vez do jogador" ++ indiceJogador
+    inputAtaque
 
+
+inputAtaque::IO()
+inputAtaque = do
+    print "Voce deseja atacar? (1)Sim (0)Não"
+    resposta <- readLn::IO Int
+    if resposta == 1 then do 
+        print "chamada de ataque" 
+        inputAtaque
+    else
+        return
+
+
+-- mapa, indicePaisAtacante, indiceAlvo, qtdExercitos
+atacar::[[Int]]->Int->Int->Int->[[Int]]
+
+
+
+ataque::
 
