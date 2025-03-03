e835cf2c674e16df9dba384f86529949220a1189
oscarhtb <oscar.henrique.torreao.vilar.brito@ccc.ufcg.edu.br>
2025-03-03 10:55:13 -0300

    objetivos

---
+++ src/ChecagemDeObjetivos.hs
@@ -0,0 +1,91 @@
+module ChecagemDeObjetivos where
+
+-- checa os objetivos de todos os jogadores, retorna o índice do vencedor ou -1
+checagemVitoria::[Int]->[Int]->[[Int]]->Int
+checagemVitoria jogadoresInfo listaDeObjetivos mapa =
+    checagemVitoriaRec (transformacaoDeLista jogadoresInfo) listaDeObjetivos mapa
+
+checagemVitoriaRec::[Int]->[Int]->[[Int]]->Int
+checagemVitoriaRec [] obj mapa = -1
+checagemVitoriaRec (h:t) (head:tail) mapa =
+    if checaObjetivo head h mapa then h
+    else checagemVitoriaRec t tail mapa
+
+
+
+checaObjetivo numObjetivo indiceJogador mapa =
+    (funcoesDeObjetivo !! (numObjetivo - 1)) indiceJogador mapa
+
+-- transforma jogadores info em uma lista que representa os indices dos jogadores
+transformacaoDeLista::[Int]->[Int]
+transformacaoDeLista jogadoresInfo = [1 .. (sum jogadoresInfo)]
+
+checa14territorios::Int->[[Int]]->Bool
+checa14territorios indiceJogador mapa = 
+    contagemDeTerritorios indiceJogador mapa >= 14
+
+checa12territoriosComDois::Int->[[Int]]->Bool
+checa12territoriosComDois indiceJogador mapa =
+    contagemDeTerritoriosComMaisDeUmExercito >= 12
+
+checaAmericaOceania::Int->[[Int]]->Bool
+checaAmericaOceania indiceJogador mapa =
+    (checaAmerica indiceJogador mapa) && (checaOceania indiceJogador mapa)
+
+checaAsiaEuropa::Int->[[Int]]->Bool
+checaAsiaEuropa indiceJogador mapa =
+    (checaAsia indiceJogador mapa) && (checaEuropa indiceJogador mapa)
+
+checaEuropaOceaniaAfrica::Int->[[Int]]->Bool
+checaEuropaOceaniaAfrica indiceJogador mapa =
+    (checaEuropa indiceJogador mapa) && (checaOceania indiceJogador mapa) && (checaAfrica indiceJogador mapa)
+
+checaAmericaEuropa::Int->[[Int]]->Bool
+checaAmericaEuropa indiceJogador mapa =
+    (checaAmerica indiceJogador mapa) && (checaEuropa indiceJogador mapa)
+
+
+funcoesDeObjetivo::[Int->[[Int]]->Bool]
+funcoesDeObjetivo = 
+[
+    checa14territorios,
+    checa12territoriosComDois,
+    checaAmericaOceania,
+    checaAsiaEuropa,
+    checaEuropaOceaniaAfrica,
+    checaAmericaEuropa
+]
+
+
+contagemDeTerritorios::Int->[[Int]]->Int
+contagemDeTerritorios indiceJogador [] = 0
+contagemDeTerritorios indiceJogador (h:t) =
+    if (h !! 0) == indiceJogador then (1 + (contagemDeTerritorios indiceJogador t))
+    else contagemDeTerritorios indiceJogador t
+
+contagemDeTerritoriosComMaisDeUmExercito::Int->[[Int]]->Int
+contagemDeTerritoriosComMaisDeUmExercito indiceJogador [] = 0
+contagemDeTerritoriosComMaisDeUmExercito indiceJogador (h:t) =
+    if (h !! 0) && ((h !! 1) > 1) == indiceJogador then (1 + (contagemDeTerritoriosComMaisDeUmExercito indiceJogador t))
+    else contagemDeTerritoriosComMaisDeUmExercito indiceJogador t
+
+
+checaAmerica::Int->[[Int]]->Bool
+checaAmerica indiceJogador mapa =
+    ((mapa !! 0) !! 0) == indiceJogador && ((mapa !! 1) !! 0) == indiceJogador && ((mapa !! 2) !! 0) == indiceJogador && ((mapa !! 3) !! 0) == indiceJogador && ((mapa !! 4) !! 0) == indiceJogador && ((mapa !! 5) !! 0) == indiceJogador && ((mapa !! 6) !! 0) == indiceJogador
+
+checaOceania::Int->[[Int]]->Bool
+checaOceania indiceJogador mapa =
+    ((mapa !! 23) !! 0) == indiceJogador && ((mapa !! 22) !! 0) == indiceJogador
+
+checaAsia::Int->[[Int]]->Bool
+checaAsia indiceJogador mapa =
+    ((mapa !! 10) !! 0) == indiceJogador && ((mapa !! 16) !! 0) == indiceJogador && ((mapa !! 17) !! 0) == indiceJogador && ((mapa !! 18) !! 0) == indiceJogador && ((mapa !! 19) !! 0) == indiceJogador && ((mapa !! 20) !! 0) == indiceJogador && ((mapa !! 21) !! 0) == indiceJogador
+
+checaEuropa::Int->[[Int]]->Bool
+checaEuropa indiceJogador mapa =
+    ((mapa !! 7) !! 0) == indiceJogador && ((mapa !! 8) !! 0) == indiceJogador && ((mapa !! 9) !! 0) == indiceJogador
+
+checaAfrica::Int->[[Int]]->Bool
+checaAfrica indiceJogador mapa =
+    ((mapa !! 11) !! 0) == indiceJogador && ((mapa !! 12) !! 0) == indiceJogador && ((mapa !! 13) !! 0) == indiceJogador && ((mapa !! 14) !! 0) == indiceJogador && ((mapa !! 15) !! 0)
--- src/Movimento.hs
+++ src/Movimento.hs
@@ -1,4 +1,5 @@
 module Movimento where
+import Text.Read (readMaybe)
 import MapeamentoTerritorios (mapeiaTerritorio)
 import Adjacencia (checarAdjacencia, matrizAdjacencia)
 import PosicionamentoInicial (substituirSublista)
@@ -7,36 +8,37 @@ import PosicionamentoInicial (substituirSublista)
 inputMovimento::[[Int]]->Int->[Int]->IO [[Int]]
 inputMovimento mapa indiceJogador movimentosFeitos = do
     putStrLn "Você deseja mover algum exercito? Sim(1) Nao(0)"
-    resposta <- readLn :: IO Int
-    if (resposta /= 1) && (resposta /= 0) then do
+    inputUsuario <- getLine
+    if not (ehInteiro inputUsuario) then do
         putStrLn "Entrada inválida :("
         inputMovimento mapa indiceJogador movimentosFeitos
-    else if resposta == 1 then do
-        putStrLn "De qual territorio voce deseja transferir os exercitos?"
-        res <- getLine
-        if ((mapeiaTerritorio res) == -1) || not (pertence mapa (mapeiaTerritorio res) indiceJogador) || movimentoJaRealizado movimentosFeitos (mapeiaTerritorio res) then do
+    else
+        let resposta = read inputUsuario :: Int
+
+        in if (resposta /= 1) && (resposta /= 0) then do
             putStrLn "Entrada inválida :("
             inputMovimento mapa indiceJogador movimentosFeitos
-        else do
-            putStrLn "E para qual territorio desejas transferir?"
-            res2 <- getLine
-            if ((mapeiaTerritorio res2) == -1) || not (pertence mapa (mapeiaTerritorio res2) indiceJogador) || not (checarAdjacencia (mapeiaTerritorio res) (mapeiaTerritorio res2) matrizAdjacencia) then do
+        else if resposta == 1 then do
+            putStrLn "De qual territorio voce deseja transferir os exercitos?"
+            res <- getLine
+            if ((mapeiaTerritorio res) == -1) || not (pertence mapa (mapeiaTerritorio res) indiceJogador) || movimentoJaRealizado movimentosFeitos (mapeiaTerritorio res) then do
                 putStrLn "Entrada inválida :("
                 inputMovimento mapa indiceJogador movimentosFeitos
             else do
-                putStrLn "E quantos exercitos serão transferidos? (min: 1, max: 3)"
-                qtd <- readLn :: IO Int
-                if (qtd < 1) || (qtd > 3) || not (possuiExercitosSuficientes mapa (mapeiaTerritorio res) indiceJogador) then do
+                putStrLn "E para qual territorio desejas transferir?"
+                res2 <- getLine
+                if ((mapeiaTerritorio res2) == -1) || not (pertence mapa (mapeiaTerritorio res2) indiceJogador) || not (checarAdjacencia (mapeiaTerritorio res) (mapeiaTerritorio res2) matrizAdjacencia) then do
                     putStrLn "Entrada inválida :("
                     inputMovimento mapa indiceJogador movimentosFeitos
-                else
-                    inputMovimento (realizaMovimento mapa (mapeiaTerritorio res) (mapeiaTerritorio res2) qtd) indiceJogador (movimentosFeitos ++ [(mapeiaTerritorio res)])
-
-
-
-
-
-    else return (mapa)
+                else do
+                    putStrLn "E quantos exercitos serão transferidos? (min: 1, max: 3)"
+                    qtd <- readLn :: IO Int
+                    if (qtd < 1) || (qtd > 3) || not (possuiExercitosSuficientes mapa (mapeiaTerritorio res) indiceJogador) then do
+                        putStrLn "Entrada inválida :("
+                        inputMovimento mapa indiceJogador movimentosFeitos
+                    else
+                        inputMovimento (realizaMovimento mapa (mapeiaTerritorio res) (mapeiaTerritorio res2) qtd) indiceJogador (movimentosFeitos ++ [(mapeiaTerritorio res)])
+        else return (mapa)
 
 possuiExercitosSuficientes::[[Int]]->Int->Int->Bool
 possuiExercitosSuficientes mapa territorio qtd =
@@ -53,3 +55,8 @@ movimentoJaRealizado movimentos territorio =
 realizaMovimento::[[Int]]->Int->Int->Int->[[Int]]
 realizaMovimento mapa origem destino qtd =
     substituirSublista (substituirSublista mapa destino [(mapa !! (destino - 1)) !! 0, ((mapa !! (destino - 1)) !! 1) + qtd]) origem [(mapa !! (origem - 1)) !! 0, ((mapa !! (origem - 1)) !! 1) - qtd]
+
+ehInteiro :: String -> Bool
+ehInteiro s = case readMaybe s :: Maybe Int of
+                Just _  -> True
+                Nothing -> False
